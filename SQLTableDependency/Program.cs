38abd2e25c79920e48e620aad0eb3983161258b0
Ahmed F. Elsheikh <elshiekh5@gmail.com>
2019-07-10 11:58:09 +0300

    Inintial commit

--- SQLTableDependency/Program.cs
+++ SQLTableDependency/Program.cs
@@ -3,13 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base;
+using TableDependency.SqlClient.Base.EventArgs;
 
 namespace SQLTableDependency
 {
-    class Program
+    public class Customer
     {
-        static void Main(string[] args)
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+    public class Program
+    {
+        //private static string _con = "data source=.; initial catalog=SQLTableDependency ; integrated security=True";
+        //private static string _con = "data source=.; initial catalog=SQLTableDependency ; integrated security=True";
+        private static string _con = "data source=.; Initial Catalog=SQLTableDependency;Integrated Security=false;User ID=sa;Password=$$123456;";
+        public static void Main()
         {
+            // The mapper object is used to map model properties 
+            // that do not have a corresponding table column name.
+            // In case all properties of your model have same name 
+            // of table columns, you can avoid to use the mapper.
+            var mapper = new ModelToTableMapper<Customer>();
+            mapper.AddMapping(c => c.Surname, "Surname");
+            mapper.AddMapping(c => c.Name, "Name");
+
+            // Here - as second parameter - we pass table name: 
+            // this is necessary only if the model name is different from table name 
+            // (in our case we have Customer vs Customers). 
+            // If needed, you can also specifiy schema name.
+            using (var dep = new SqlTableDependency<Customer>(
+                connectionString: _con,
+
+                tableName: "Customers", mapper: mapper, executeUserPermissionCheck:false))
+            {
+                dep.OnChanged += Changed;
+                dep.Start();
+
+                Console.WriteLine("Press a key to exit");
+                Console.ReadKey();
+
+                dep.Stop();
+            }
+        }
+
+        public static void Changed(object sender, RecordChangedEventArgs<Customer> e)
+        {
+            var changedEntity = e.Entity;
+
+            Console.WriteLine("DML operation: " + e.ChangeType);
+            Console.WriteLine("ID: " + changedEntity.Id);
+            Console.WriteLine("Name: " + changedEntity.Name);
+            Console.WriteLine("Surname: " + changedEntity.Surname);
         }
     }
 }
